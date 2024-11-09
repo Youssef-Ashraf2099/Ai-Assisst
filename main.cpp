6bc2b33933abdb810c9954e60872c3f4a715ba1b
Youssef-Ashraf2099 <youssef.ashraf@student.giu-uni.de>
2024-11-09 19:37:53 +0200

    basic responsive function to user added

--- main.cpp
+++ main.cpp
@@ -8,8 +8,17 @@ void toLowerCase(string &str) {
     transform(str.begin(), str.end(), str.begin(), ::tolower);
 }
 
-
-
+void respondToUser(const string &input) {
+    if (input.find("hello") != string::npos) {
+        cout << "Assistant: Hello! How can I help you today?" << endl;
+    } else if (input.find("how are you") != string::npos) {
+        cout << "Assistant: I'm just a program, but I'm doing great! How can I assist you?" << endl;
+    } else if (input.find("bye") != string::npos) {
+        cout << "Assistant: Goodbye! Have a great day!" << endl;
+    } else {
+        cout << "Assistant: I'm not sure how to respond to that. Could you try rephrasing?" << endl;
+    }
+}
 int main() {    
     string userInput;
 
