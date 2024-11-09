35df2a78f9734465aeb13ebf6d24f488caa2bb4a
Youssef-Ashraf2099 <youssef.ashraf@student.giu-uni.de>
2024-11-09 19:27:04 +0200

    first base code

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm> // For transform function
+
+using namespace std;
+
+
+
+
+int main() {    
+    string userInput;
+
+    cout << "Interactive Assistant: Type 'bye' to exit." << endl;
+    while (true) {
+        // get user input
+    }
+
+    return 0;
+}
